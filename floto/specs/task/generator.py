74930caca487642deb6c22b3dc8a1c3236b421b2
philipppahl <ph.pahl@gmail.com>
2017-01-23 21:25:07 +0100

    Bugfix: Task list parameter

--- floto/specs/task/generator.py
+++ floto/specs/task/generator.py
@@ -3,10 +3,10 @@ from floto.specs.task import ActivityTask
 
 class Generator(ActivityTask):
     def __init__(self, *, domain, name, version, id_=None, requires=None, input=None,
-                 retry_strategy=None):
+            retry_strategy=None, task_list):
         """Defines an activity task which generates <floto.specs.Task> objects. The generated tasks
         are added to the execution logic of the decider.
         """
         super().__init__(domain=domain, name=name, version=version, id_=id_, requires=requires,
-                         input=input, retry_strategy=retry_strategy)
+                         input=input, retry_strategy=retry_strategy, task_list=task_list)
 
